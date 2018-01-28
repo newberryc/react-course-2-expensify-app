0c0c8f155967b538d26dfe76a68f9d65e5debdfd
Chuck Newberry <chuck.newbs@gmail.com>
2018-01-27 20:08:42 -0600

    remove from database fixes

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -30,15 +30,15 @@ export const startAddExpense = (expenseData = {}) => {
 };
 
 // REMOVE_EXPENSE
-export const removeExpense = ({id} = {}) => ({
+export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
 
-export const startRemoveExpense = ({id} = {}) => {
+export const startRemoveExpense = ({ id } = {}) => {
     return (dispatch) => {
         return database.ref(`expenses/${id}`).remove().then(() => {
-            dispatch(removeExpense({id}));
+            dispatch(removeExpense({ id }));
         });
     };
 };
